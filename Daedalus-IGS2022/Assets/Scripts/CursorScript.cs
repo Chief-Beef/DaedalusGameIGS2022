ade7dba8696a5fe615544c41a52f2f1b443f493a
Chief-Beef <wschweri1@ltu.edu>
2022-01-19 17:23:33 -0500

    Crosshair changes
    crosshair size needs to be set to 32 from the original 2048

--- Daedalus-IGS2022/Assets/Scripts/CursorScript.cs
+++ Daedalus-IGS2022/Assets/Scripts/CursorScript.cs
@@ -5,18 +5,55 @@ using UnityEngine;
 public class CursorScript : MonoBehaviour
 {
 
-    public Texture2D baseCursor;
-    public Vector2 mouseOffset = new Vector2(12.5f,12.5f); 
+    public Texture2D grappleCursor;
+    public Texture2D fireGun;
+    public Texture2D iceGun;
+    public Texture2D poisonGun;
+
+    public Texture2D[] cursors;
+
+    public int active;
+
+    public KeyCode switchWeapon;
+    public KeyCode grappleButton;
+
+    public Vector2 mouseOffset = new Vector2(12.5f,12.5f);
+
+    //Click on the cursor you want to use, go to max size and change it from 2048 to 32
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.SetCursor(baseCursor, mouseOffset, CursorMode.ForceSoftware);
+        //order can be changed who cares
+        cursors[0] = fireGun;
+        cursors[1] = iceGun;
+        cursors[2] = poisonGun;
+      
+        active = 0;
+
+        //on start grapple active, can change to whatever
+        Cursor.SetCursor(grappleCursor, mouseOffset, CursorMode.ForceSoftware);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        //when you switch weapons it changes the cursor
+        //active%3 to remove index out of bounds error
+
+        if(Input.GetKeyDown(switchWeapon))
+        {
+            active = (active + 1) % 3;
+            Cursor.SetCursor(cursors[active], mouseOffset, CursorMode.ForceSoftware);
+            //Audio.play for weapon swap noise
+        }
+
+        if (Input.GetKeyDown(grappleButton))
+        {
+            Cursor.SetCursor(grappleCursor, mouseOffset, CursorMode.ForceSoftware);
+        }
+
     }
+
 }
