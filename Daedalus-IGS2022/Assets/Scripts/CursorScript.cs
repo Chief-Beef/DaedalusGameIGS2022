8d6b205fd04ca2d003d8710d26da731b4f34373f
Chief-Beef <wschweri1@ltu.edu>
2022-01-19 16:23:40 -0500

    CrosshairUpdate Hotfix

---
+++ Daedalus-IGS2022/Assets/Scripts/CursorScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+
+    public Texture2D baseCursor;
+    public Vector2 mouseOffset = new Vector2(12.5f,12.5f); 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(baseCursor, mouseOffset, CursorMode.ForceSoftware);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
