de93f27a417d8208b57ade616f78e0968f908ef0
csjbennett <60185886+csjbennett@users.noreply.github.com>
2022-01-24 13:42:26 -0500

    Added some stuff
    Added test enemy and test enemy script, started work on adding bullet functionality but never finished. Weapon wheel still needs fixing.

---
+++ Daedalus-IGS2022/Assets/Scripts/Weapon Wheel Scripts/WeaponWheelButtonController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class WeaponWheelButtonController : MonoBehaviour
+{
+    public int Id;
+    private Animator anim;
+    public string itemName;
+    public TextMeshProUGUI itemText;
+    public Image selectedItem;
+    private bool selected = false;
+    public Sprite icon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selected)
+        {
+            selectedItem.sprite = icon;
+            itemText.text = itemName;
+
+        }
+    }
+
+    public void Selected()
+    {
+        selected = true;
+    }
+
+    public void DeSelected()
+    {
+        selected = false;
+    }
+
+    public void hoverEnter()
+    {
+        anim.SetBool("Hover", true);
+        itemText.text = itemName;
+    }
+
+    public void hoverExit()
+    {
+        anim.SetBool("Hover", false);
+        itemText.text = "";
+    }
+}
