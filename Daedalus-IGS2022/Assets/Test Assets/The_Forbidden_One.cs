08983d17f81a315870f8f5d8cea0bf691ab764f0
csjbennett <cbennett1@ltu.edu>
2022-01-25 15:43:21 -0500

    Moved files around + added test enemies
    The forbidden one has been added
    Added a test sword for the player (keybinds are not set up for it yet, so the hitbox is out at all times)

--- Daedalus-IGS2022/Assets/Test Assets/The_Forbidden_One.cs
+++ Daedalus-IGS2022/Assets/Test Assets/The_Forbidden_One.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class The_Forbidden_One : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Daedalus-IGS2022/Assets/Test Assets/Weakspot_Of_The_Forbidden_One.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weakspot_Of_The_Forbidden_One : MonoBehaviour
+{
+    public GameObject parentObject;
+
+    private void OnTriggerEnter2D(Collider2D trigger)
+    {
+        if (trigger.gameObject.tag == "PlayerAttack")
+        {
+            Destroy(parentObject.gameObject);
+        }
+    }
+}
