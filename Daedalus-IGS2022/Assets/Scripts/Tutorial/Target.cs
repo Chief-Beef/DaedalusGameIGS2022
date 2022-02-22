2fecb9638ad5aa218544a8bedf603ed2f4186446
csjbennett <cbennett1@ltu.edu>
2022-02-21 20:36:08 -0500

    More work done on tutorial level
    Added targets that will open a barrier when destroyed

---
+++ Daedalus-IGS2022/Assets/Scripts/Tutorial/Target.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public Target_Counter tc;
+
+    private void OnTriggerEnter2D(Collider2D trigger)
+    {
+        if (trigger.gameObject.tag == "PlayerAttack")
+        {
+            AddToCounter();
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void AddToCounter()
+    {
+        tc.AddTargetDown();
+    }
+}
---
+++ Daedalus-IGS2022/Assets/Scripts/Tutorial/Target_Counter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Target_Counter : MonoBehaviour
+{
+    public int targetCount;
+    private int currentCount = 0;
+
+    public Text targetCounter;
+
+    public SpriteRenderer barrier;
+    public SpriteRenderer mask;
+    public GameObject barrierObj;
+    private float opacity = 1.0f;
+    private bool completed;
+
+    public Sequencer stageSequencer;
+
+    private void Awake()
+    {
+        targetCounter.text = currentCount.ToString() + "/" + targetCount.ToString() + " targets destroyed";
+    }
+
+    private void Update()
+    {
+        if (opacity > 0 && completed)
+        {
+            opacity -= Time.deltaTime;
+            barrier.color = new Color(1, 1, 1, opacity);
+            mask.color = new Color(1, 1, 1, opacity);
+        }
+        else if (completed)
+        {
+            Destroy(barrierObj);
+            Destroy(mask);
+            completed = false;
+        }
+    }
+
+    public void AddTargetDown()
+    {
+        currentCount++;
+
+        targetCounter.text = currentCount.ToString() + "/" + targetCount.ToString() + " targets destroyed";
+
+        if (currentCount == targetCount)
+        {
+            completed = true;
+        }
+    }
+}
