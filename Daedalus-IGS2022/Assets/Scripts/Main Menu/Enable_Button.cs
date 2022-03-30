abebdbe1052e4c4eb31bca1156e54f05c7e5100d
Cameron S. Bennett <cbennett1@ltu.edu>
2022-03-30 15:38:02 -0400

    Added new font and menus

---
+++ Daedalus-IGS2022/Assets/Scripts/Main Menu/Enable_Button.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enable_Button : MonoBehaviour
+{
+    // This script enables or disables a button based on how far the player has progressed
+    // Meant to be used on level select screen to disable levels past what player has unlocked
+
+    // Attach this script to a UI element that blocks the button behind it
+
+    // Player must be this far in the game to select the button
+    public int requiredLevel;
+
+    private void Start()
+    {
+        // Progression int is used to keep track of how far player has progressed
+        int progression = PlayerPrefs.GetInt("Progression", 0);
+
+        if (requiredLevel >= progression)
+        {
+            this.gameObject.SetActive(false);
+        }
+        else
+            this.gameObject.SetActive(true);
+    }
+}
